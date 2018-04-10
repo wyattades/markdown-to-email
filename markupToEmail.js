881b2f6241e758bf5d10f3043c0f2eb35c126a00
Wyatt Ades <wyattades@gmail.com>
2018-04-10 14:58:00 -0700

    started

--- markupToEmail.js
+++ markupToEmail.js
@@ -53,31 +53,130 @@ ${items.join('')}\
 </ul>`;
   };
 
+  const formatLine = (line) => {
+    // Replace bold
+    // Replace italics
+    return line.trim();
+  };
+
+
   const markupToEmail = (markup) => new Promise((resolve, reject) => {
 
     markup = markup.trim();
     if (markup.length === 0) return resolve('');
 
-    const sections = markup
-      .replace(/\r/g, '')
-      .replace(/<(\/?)b>/g, (match, closer) => `<${closer}strong>`)
-      .replace(/\[(.*?)\]\{(.+?)\}/g, (match, label, url) => {
-        if (label.trim().length === 0) label = url;
-        return $link(label, url);
-      })
-      .split('\n\n\n');
+    const lines = markup.split('\n').map(formatLine);
 
-    const pieces = [];
+    let pieces = [];
 
-    for (let section of sections) {
-      const lines = section.split('\n\n');
 
-      const [ str, notList ] = ender(lines[0], '-');
-      lines[0] = str;
-
-      pieces.push($title(lines[0]));
-      pieces.push($list(lines.slice(1), notList));
+    let section;
+    const sectionsRegex = /[\t ]*(\#|\+|(?:[\n\r]{2,}))[\t ]*([^\n\r]*)[\n\r]{2,}/g;    
+    while((section = sectionsRegex.exec(markup)) !== null) {
+      console.log(section);
+      const type = section[1].length === 1 && section[1];
+      const sectionText = section[2].replace(/[\n\r]+/, '<br/>');
+      pieces.push(`<p><strong>${type}</strong><br/>${sectionText}</p>`)
     }
+
+    let paragraph = [];
+    let prev = 'blank'; // null, para, bullet
+
+    for (let i = 0; i < lines.length; i++) {
+      let line = lines[i].trim();
+
+      let current;
+      if (line.length === 0) current = 'blank';
+      else if (isHeader(line)) current = 'header';
+      else if (isBulletHead(line)) current = 'bullethead';
+      else current = 'line';
+
+      if (current !== 'blank') line = formatLine(line);
+
+      if (current === 'bullethead') {
+
+      }
+
+      if (current === 'line') {
+        if (prev !== null) {
+          paragraph.push(line);
+        }
+      } else {
+        if (prev === 'bullet') {
+          pieces.push(formatBullet(paragraph));
+          paragraph = [];
+        } else if (prev === 'para') {
+          pieces.push(formatParagraph(paragraph));
+          paragraph = [];
+        }
+
+        if (current === )
+      }
+
+      if (line.length === 0) {
+        // blank
+        prev = 'blank';
+        // if (prev === 'listhead' || prev === 'listel') {
+        //   if (list.length > 0) {
+        //     pieces.push(formatList(list));
+        //     list = [];
+        //   }
+        // } else if (prev === 'listel') {
+          
+        // } else if (prev === 'para') {
+        //   if (paragraph.length > 0) {
+        //     pieces.push(formatParagraph(paragraph));
+        //     paragraph = [];
+        //   }
+        // } else if (prev === 'blank') {
+        //   // do nothing
+        // }
+      } else if (isListElement(line)) {
+        isList = true;
+        list.push([ line ]);
+      } else if (isHeader(line)) {
+        list.push(formatHeader(line));
+      } else {
+        if (prev === 'listhead') {
+          
+        } else if (prev === 'listel') {
+          if (list.length > 0) {
+            pieces.push(formatList(list));
+            list = [];
+          }
+        } else if (prev === 'listel') {
+          
+        } else if (prev === 'para') {
+          if (paragraph.length > 0) {
+            pieces.push(formatParagraph(paragraph));
+            paragraph = [];
+          }
+        } else if (prev === 'blank') {
+          // do nothing
+        }
+      }
+    }
+
+    // const sections = markup
+    //   .replace(/\r/g, '')
+    //   .replace(/<(\/?)b>/g, (match, closer) => `<${closer}strong>`)
+    //   .replace(/\[(.*?)\]\{(.+?)\}/g, (match, label, url) => {
+    //     if (label.trim().length === 0) label = url;
+    //     return $link(label, url);
+    //   })
+    //   .split('\n\n\n');
+
+    // const pieces = [];
+
+    // for (let section of sections) {
+    //   const lines = section.split('\n\n');
+
+    //   const [ str, notList ] = ender(lines[0], '-');
+    //   lines[0] = str;
+
+    //   pieces.push($title(lines[0]));
+    //   pieces.push($list(lines.slice(1), notList));
+    // }
 
     const result = pieces.join('');
 
