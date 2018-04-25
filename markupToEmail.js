b3f7efb7c3dacb694ab1f485ae6ed0cfcdba50e0
Wyatt Ades <wyattades@gmail.com>
2018-04-25 14:48:39 -0700

    Remove old parser

--- markupToEmail.js
+++ markupToEmail.js
@@ -1,111 +1,47 @@
 (() => {
 
-  // List types
-  const INDENTED = 0,
-        UNINDENTED = 1,
-        BULLETED = 2;
-  
-  const styles = 'color:#444444;margin:0 0 10px 0;padding:0;font-size:10pt;font-family:Open Sans,sans-serif;font-weight:400';
-
-  const ender = (str, end) => {
-    if (str.endsWith(end)) {
-      return [ str.substr(0, str.length - end.length), true ];
-    }
-    return [ str, false ];
+  const STYLES = 'color:#444444;margin:0 0 10px 0;padding:0;font-size:10pt;font-family:Open Sans,sans-serif;font-weight:400';  
+  const SHOWDOWN_SCRIPT = 'https://cdnjs.cloudflare.com/ajax/libs/showdown/1.8.6/showdown.min.js';
+  const OPTIONS = {
+    noHeaderId: true,
+    tables: false,
+    simpleLineBreaks: true,
+    encodeEmails: false,
   };
 
-  const addBreaks = (str) => {
-    return str.split('\n').map(s => {
-      const [ _str, shouldBold ] = ender(s, '+');
-      return shouldBold ? `<strong>${_str}</strong>` : _str;
-    }).join('<br/>');
-  };
-
-  // There can be no '\n' in the following string
-  const $link = (label, url) => `<a href="${url}" target="_blank">${label}</a>`;
-    
-  const $title = str => `\
-<p style="${styles}text-align:justify">\
-<strong>${addBreaks(str)}</strong>\
-</p>`;
-
-  const $list = (strs, type) => {
-
-    if (strs.length === 0) {
-      console.log('Must provide atleast one string');
-      return '';
-    }
-
-    const items = [];
-
-    if (type !== BULLETED) {
-      for (let str of strs) {
-        items.push(`\
-<p style="${styles}">\
-${addBreaks(str)}\
-</p>`);
-      }
-    } else {
-      for (let str of strs) {
-        items.push(`\
-<li style="${styles}">\
-${addBreaks(str)}\
-</li>`);
-      }
-    }
-
-    if (type === UNINDENTED) {
-      return items.join('');
-    } else {
-      return `\
-<ul>\
-${items.join('')}\
-</ul>`;
-    }
-
-  };
-
-  const markupToEmail = (markup) => new Promise((resolve, reject) => {
-
-    markup = markup.trim();
-    if (markup.length === 0) return resolve('');
-
-    const sections = markup
-      .split(/\n/).map((line) => line.trim()).join('\n')
-      // .replace(/\r/g, '')
-      .replace(/<(\/?)b>/g, (match, closer) => `<${closer}strong>`)
-      .replace(/\[(.*?)\]\{(.+?)\}/g, (match, label, url) => {
-        if (label.trim().length === 0) label = url;
-        return $link(label, url);
-      })
-      .split('\n\n\n');
-
-    const pieces = [];
-
-    for (let section of sections) {
-      const lines = section.split('\n\n');
-
-      let type = BULLETED,
-          hasEnder;
-      const backup = lines[0];
-      [ lines[0], hasEnder ] = ender(lines[0], '-!');
-      if (hasEnder) {
-        type = INDENTED;
-      } else {
-        [ lines[0], hasEnder ] = ender(lines[0], '-');
-        if (hasEnder) type = UNINDENTED;
-      }
-      console.log(backup, type);
-
-      pieces.push($title(lines[0]));
-      pieces.push($list(lines.slice(1), type));
-    }
-
-    const result = pieces.join('');
-
-    resolve(result);
-    
+  const loadScriptAsync = (uri) => new Promise((resolve, reject) => {
+    const el = document.createElement('script');
+    el.src = uri;
+    el.async = true;
+    el.onload = () => resolve();
+    document.head.appendChild(el);
   });
+
+  let converter;
+
+  const markupToEmail = (markup) => 
+    (converter ? Promise.resolve() : loadScriptAsync(SHOWDOWN_SCRIPT).then(() => {
+      converter = new window.showdown.Converter(OPTIONS);
+    }))
+    .then(() => {
+      return converter.makeHtml(markup)
+      .replace(/<(p|li)>/g, (_, tag) => `<${tag} style="${STYLES}">`)            
+      .replace(/<pre>\s*<code>(.*?)<\/code>\s*<\/pre>/gs, (_, text) => {
+        const paragraphs = text
+        .trim()
+        .split(/\n{2,}/)
+        .map((t) => `<p style="${STYLES}">${t.replace(/\n/g, '<br/>')}</p>`)
+        .join('');
+        
+        return `<ul>${paragraphs}</ul>`
+      })
+      .replace(/<(\/?)h[1-6]>/g, (_, closer) => {
+        return !closer ?
+          `<p style="${STYLES};text-align:justify"><strong>` :
+          `</strong></p>`;
+      })
+      // .replace('\n', '');
+    });
 
   if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
     module.exports = markupToEmail;
